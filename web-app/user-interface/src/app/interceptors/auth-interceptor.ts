03da84bc5e064c5b956fa30f611644756993f988
Alexfordevops <matheusalexandre.in@gmail.com>
2025-08-31 10:09:54 -0300

    Feat: JWT expired verification when go to protected routes with back verification

--- web-app/user-interface/src/app/interceptors/auth-interceptor.ts
+++ web-app/user-interface/src/app/interceptors/auth-interceptor.ts
@@ -1,19 +1,33 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpRequest, HttpHandlerFn, HttpEvent, HttpErrorResponse } from '@angular/common/http';
 import { inject } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { AuthService } from '../services/auth-service/auth-service';
+import { Router } from '@angular/router';
 
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
-
+export const authInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> => {
   const authService = inject(AuthService);
+  const router = inject(Router);
   const token = authService.getToken();
 
-  if(token){
-    const cloned = req.clone({
-      setHeaders:{
+  let clonedReq = req;
+  if (token) {
+    clonedReq = req.clone({
+      setHeaders: {
         Authorization: `Bearer ${token}`
       }
     });
-    return next(cloned);
   }
-  return next(req);
+
+  return next(clonedReq).pipe(
+    // Se receber 401 (Unauthorized) do backend, significa que o token expirou ou é inválido
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        authService.logout(); // remove token
+        router.navigate(['/login']); // redireciona para login
+      }
+      return throwError(() => error);
+    })
+  );
 };
+
